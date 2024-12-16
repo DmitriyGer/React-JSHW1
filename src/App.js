2e6f24f8d5df852c32ddc5066c894253a9d586b5
Dmitriy <dim1a15@mail.ru>
2024-12-16 14:12:15 +0300

    готовая работа

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Message from './Message';
+
+function App() {
+  return (
+    <div className="App">
+      <Message text="Hello, this is a message component!" />
+    </div>
+  );
+}
+
+export default App;
